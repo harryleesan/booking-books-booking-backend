4a1ff2cecb11c6d2266e30d30992b4cf582e189f
harryleesan <harry.lee.san.temp@gmail.com>
2018-12-28 20:42:28 +0200

    Added flask-cors to enable CORS

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from prometheus_flask_exporter import PrometheusMetrics
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 metrics = PrometheusMetrics(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
